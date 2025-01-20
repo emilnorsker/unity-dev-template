cee3777c450971be7edeb19e2a7ab9ff3018da8c
norsker <emilgraaenorsker@gmail.com>
2025-01-20 16:23:35 +0100

    feat(test): Add initial math test example

---
+++ UnityProject/Assets/Tests/TestExample.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class TestExample
+{
+    [Test]
+    public void SimpleAddition_WhenAddingOneAndOne_ReturnsTwo()
+    {
+        // Arrange
+        int a = 1;
+        int b = 1;
+        
+        // Act
+        int result = a + b;
+        
+        // Assert
+        Assert.That(result, Is.EqualTo(2), "Basic addition of 1 + 1 should equal 2");
+    }
+}
